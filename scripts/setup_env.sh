5793d659bf173eaabf8d2e3a9a787882a6cf813e
Jui-Hsien Wang <juiwang@adobe.com>
2022-04-22 14:31:26 -0700

    setup env scripts for pose
    Former-commit-id: 5e1f4e394cc44e092caf71704b227fdda8ad8c49
    Former-commit-id: 433d3c9ca9ea47d6be21829c2cdb67c0ec3fa8a8

---
+++ scripts/setup_env.sh
@@ -0,0 +1,16 @@
+## working versions:
+# pytorch: 1.11.0
+# cuda: 11.3
+# mmcv-full: 1.5.0
+# mmdet: 2.23.0
+# numpy: 1.22.3
+
+conda install pytorch=1.11.0 torchvision torchaudio cudatoolkit=11.3 -c pytorch
+
+pip install mmcv-full -f https://download.openmmlab.com/mmcv/dist/cu113/torch1.11.0/index.html
+
+pip install openmim
+mim install mmdet
+
+# this is needed because of some weird numpy linking issues
+pip install -U numpy
