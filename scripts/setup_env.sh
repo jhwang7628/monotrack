9b27f66e3ccd1441e5f71514efd5bf86c46d190e
Jui-Hsien Wang <juiwang@adobe.com>
2022-04-25 10:45:23 -0700

    cudnn and file limits
    Former-commit-id: b36f9bc4a1486830f79dde40b29cc28d4019ce21
    Former-commit-id: 0d61a01ef8d67f8a27ebf9b72ff95656661da975

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -7,6 +7,7 @@
 ### mmcv-full: 1.5.0
 ### mmdet: 2.23.0
 ### numpy: 1.22.3
+### cudnn: 8.2
 
 conda install pytorch=1.11.0 torchvision torchaudio cudatoolkit=11.3 -c pytorch
 
@@ -17,3 +18,8 @@ mim install mmdet
 
 # this is needed because of some weird numpy linking issues
 pip install -U numpy
+
+conda install -c conda-forge cudnn
+
+# raise the number of files that can be opened
+ulimit -n 2048
