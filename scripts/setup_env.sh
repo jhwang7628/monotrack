f2e9006c48f449562294bcb8598b3380f9ab119b
Jui-Hsien Wang <juiwang@adobe.com>
2022-05-27 10:01:45 -0700

    more packages in order to build
    Former-commit-id: 6f491486d279d7125c9db89c25e4bc74e1152b19
    Former-commit-id: 770f1b5435e60ccbebcfe226c0ba46669ae11972
    Former-commit-id: f6eb7a72474f31c91cac8c10c0bc3feac55512e2

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -9,17 +9,28 @@
 ### numpy: 1.22.3
 ### cudnn: 8.2
 
-conda install pytorch=1.11.0 torchvision torchaudio cudatoolkit=11.3 -c pytorch
+# pytorch and cuda stuff
+conda install -y pytorch=1.11.0 torchvision torchaudio cudatoolkit=11.3 -c pytorch
+sudo apt install -y nvidia-cuda-toolkit
+conda install -y -c conda-forge cudnn
 
+# mmpose
 pip install mmcv-full -f https://download.openmmlab.com/mmcv/dist/cu113/torch1.11.0/index.html
-
 pip install openmim
 mim install mmdet
 
 # this is needed because of some weird numpy linking issues
 pip install -U numpy
 
-conda install -c conda-forge cudnn
+# tf and image processing
+pip install piexif
+pip install tqdm
+pip install tensorflow
+pip install focal-loss
+conda install -y dask
 
 # raise the number of files that can be opened
-ulimit -n 2048
+ulimit -n 4096
+
+# to find libcudart.so
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/conda/envs/ai-badminton/lib/
